a08d67df18d5299c55544700a75e1ed00d2cd1f1
Yasitha Miyuranga <yasithamiyuranga219@gmail.com>
2026-01-11 20:59:48 +0530

    08_Update a tag in the tags array (Object)

--- React/src/App.tsx
+++ React/src/App.tsx
@@ -2,7 +2,11 @@ import { useState } from "react";
 import Alert from "./components/Alert";
 
 const App = () => {
-  const [tags, setTags] = useState(["tag1", "tag2", "tag3"]);
+  const [tags, setTags] = useState([
+    { id: 1, name: "tag1" },
+    { id: 2, name: "tag2" },
+    { id: 3, name: "tag3" }
+  ]);
 
   const handleClick = () => {
     // Add a new tag to the tags array
@@ -18,19 +22,19 @@ const App = () => {
 
 
     // Update a tag in the tags array
-    setTags(
-      tags.map((tag) => {
-        return tag === "tag1" ? "updatedTag1" : tag;
-      })
-    );
+    // setTags(tags.map((tag) => {return tag === "tag1" ? "updatedTag1" : tag;}));
 
+    setTags(
+      tags.map((tag) =>
+      tag.name === "tag2" ? { ...tag, name: "updatedTag2" } : tag)
+    )
     
   };
 
   return (
     <>
       {tags.map((tag, index) => {
-        return <p key={index}>{tag}</p>;
+        return <p key={index}>{tag.name}</p>;
       })}
       <button onClick={handleClick} className="border-3">
         Add Tag
