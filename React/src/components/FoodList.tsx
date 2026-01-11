c4ea143111929b022118463509cffc50572bd600
Yasitha Miyuranga <yasithamiyuranga219@gmail.com>
2026-01-11 09:05:37 +0530

    05_create_useState Hook

--- React/src/components/FoodList.tsx
+++ React/src/components/FoodList.tsx
@@ -1,27 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const FoodList = () => {
     const Foods: string[] = ['Pizza', 'Burger', 'Sushi', 'Pasta', 'Salad']
-  return (
-    <>
-      {/* And Operator Example */}
-      {Foods && (
-        <ul className="mt-10 flex gap-5">
-          {Foods.map((food: string) => (
-            <button 
-            key={food} 
-            className="font-bold text-3xl text-green-500 border-2 cursor-pointer"
-            onClick={() => {
-                console.log(food)
-            }}
-            >
-            {food}
-            </button>
-          ))}
-        </ul>
-      )}
-    </>
-  );
+    const [cars, setCars] = useState("toyota");
+
+    const handleChangeValue = (food: string) => {
+      console.log(food);
+      setCars(food);
+    }
+    return (
+      <>
+        <p className="text-2xl font-bold text-pink-600">{cars}</p>
+        {/* And Operator Example */}
+        {Foods && (
+          <ul className="mt-10 flex gap-5">
+            {Foods.map((food: string) => (
+              <button
+                key={food}
+                className="font-bold text-3xl text-green-500 border-2 cursor-pointer"
+                onClick={() => handleChangeValue(food)}
+              >
+                {food}
+              </button>
+            ))}
+          </ul>
+        )}
+      </>
+    );
 } 
 
 export default FoodList
