e4ae9587d38123384afb3a1783b6f146d2c226f3
Yasitha Miyuranga <yasithamiyuranga219@gmail.com>
2026-01-13 22:48:59 +0530

    10_feat: add multiple states and useEffect with dependency array

--- React/src/App.tsx
+++ React/src/App.tsx
@@ -2,18 +2,28 @@ import { useEffect, useState } from "react";
 
 const App = () => {
   const [count, setCount] = useState(0);
+  const [number, setNumber] = useState(10);
 
   useEffect(() => {
-    document.title = "Yasitha";
-    console.log("App");
-
-    setCount(count + 1);
-    console.log(count);
-  });
+    console.log("Count : ", count);
+    console.log("Number : ", number);
+  }, [count, number]);
 
   return (
     <>
       <h1 className="text-7xl font-bold">This is my App</h1>
+      <button
+        className="text-4xl font-bold border-2 cursor-pointer p-4 rounded-2xl"
+        onClick={() => setCount(count + 1)}
+      >
+        Click
+      </button>
+      <button
+        className="text-4xl font-bold border-2 cursor-pointer p-4 rounded-2xl"
+        onClick={() => setNumber(number + 1)}
+      >
+        Remove
+      </button>
     </>
   );
 };
